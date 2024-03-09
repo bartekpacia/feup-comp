c942fae462e138c13bedf87d971e984a7e3cd5b2
Bartek Pacia <barpac02@gmail.com>
2024-03-09 23:44:39 +0000

    remove commented out code and debug helpers

--- src/main/pt/up/fe/comp2024/symboltable/JmmSymbolTableBuilder.java
+++ src/main/pt/up/fe/comp2024/symboltable/JmmSymbolTableBuilder.java
@@ -4,8 +4,10 @@ import pt.up.fe.comp.jmm.analysis.table.Symbol;
 import pt.up.fe.comp.jmm.analysis.table.Type;
 import pt.up.fe.comp.jmm.ast.JmmNode;
 import pt.up.fe.comp2024.ast.Kind;
-import pt.up.fe.comp2024.ast.TypeUtils;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 import static pt.up.fe.comp2024.ast.Kind.METHOD_DECL;
 import static pt.up.fe.comp2024.ast.Kind.VAR_DECL;
@@ -43,25 +45,15 @@ public class JmmSymbolTableBuilder {
         return String.join(".", names);
     }
 
-    //
     private static Map<String, Type> buildReturnTypes(JmmNode classDecl) {
 
         Map<String, Type> map = new HashMap<>();
-        System.out.println("JmmSymbolTableBuilder: printing root JmmNode");
         System.out.println(classDecl.toTree());
         classDecl.getChildren(METHOD_DECL).stream()
                 .forEach(method -> {
-                    System.out.println("DEBUG: method " + method.get("name") + " has " + method.getChildren().size() + " children");
-                    method.getChildren().stream().forEach(System.out::println);
-                    System.out.println("DEBUG: method " + method.get("name") + " has " + method.getAttributes().size() + " attributes");
-                    method.getAttributes().stream().forEach(System.out::println);
-                    method.getOptional(null);
-
                     final JmmNode typeNode = method.getChildren("Type").get(0);
                     final String typeName = typeNode.get("name");
                     final boolean isArray = typeNode.get("isArray").equals("true");
-                    // // TODO(bartek): handle array types
-
                     final Type type = new Type(typeName, isArray);
                     map.put(method.get("name"), type);
                 });
@@ -80,7 +72,6 @@ public class JmmSymbolTableBuilder {
                             .forEach(param -> {
                                 final JmmNode typeNode = param.getChildren("Type").get(0);
                                 final String typeName = typeNode.get("name");
-                                // TODO(bartek): handle array types
                                 final boolean isArray = typeNode.get("isArray").equals("true");
                                 final Type type = new Type(typeName, isArray);
                                 parameters.add(new Symbol(type, param.get("name")));
@@ -88,14 +79,6 @@ public class JmmSymbolTableBuilder {
 
                     map.put(method.get("name"), parameters);
                 });
-
-        /*
-         * classDecl.getChildren(METHOD_DECL).stream()
-         * .forEach(method -> map.put(method.get("name"),
-         * method.getChildren("Param").stream().forEach(param -> param.get("name"))));
-         */
-        // method.getChildren("Param").stream().forEach(param -> new Symbol(intType,
-        // param.get("name")))
         return map;
     }
 
@@ -108,7 +91,6 @@ public class JmmSymbolTableBuilder {
         return map;
     }
 
-    // works
     private static List<String> buildMethods(JmmNode classDecl) {
         return classDecl.getChildren(METHOD_DECL).stream()
                 .map(method -> method.get("name"))
@@ -120,7 +102,6 @@ public class JmmSymbolTableBuilder {
                 .map(fieldDecl -> {
                     final JmmNode typeNode = fieldDecl.getChildren("Type").get(0);
                     final String typeName = typeNode.get("name");
-                    // TODO(bartek): handle array types
                     final boolean isArray = fieldDecl.getObject("typename",JmmNode.class).get("isArray").equals("true");
                     final Type type = new Type(typeName, isArray);
                     return new Symbol(type, fieldDecl.get("name"));
@@ -129,7 +110,6 @@ public class JmmSymbolTableBuilder {
     }
 
     private static List<Symbol> getLocalsList(JmmNode methodDecl) {
-
         return methodDecl.getChildren(VAR_DECL).stream()
                 .map(varDecl -> new Symbol(new Type(varDecl.getObject("typename",JmmNode.class).get("name"),varDecl.getObject("typename",JmmNode.class).get("isArray").equals("true")), varDecl.get("name")))
                 .toList();
