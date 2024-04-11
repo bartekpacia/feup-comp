fb69b75a404bee67cd0bd7f69b72c675a68d8593
Bartek Pacia <up202311689@fe.up.pt>
2024-04-11 15:10:04 +0100

    fix Desktop being always opened on macOS

--- src/main/pt/up/fe/comp2024/Launcher.java
+++ src/main/pt/up/fe/comp2024/Launcher.java
@@ -17,8 +17,6 @@ import java.util.Map;
 public class Launcher {
 
     public static void main(String[] args) {
-        System.setProperty("apple.awt.UIElement", "true");
-
         SpecsSystem.programStandardInit();
 
         Map<String, String> config = CompilerConfig.parseArgs(args);
