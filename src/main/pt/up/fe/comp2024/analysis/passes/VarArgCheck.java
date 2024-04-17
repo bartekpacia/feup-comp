2af0f71e55068c11a9e772d2683d883e2da6fce6
goncalo2k <goncalo.miranda.cplx@gmail.com>
2024-04-17 23:31:08 +0100

    Added vararg initial check

---
+++ src/main/pt/up/fe/comp2024/analysis/passes/VarArgCheck.java
@@ -0,0 +1,35 @@
+package pt.up.fe.comp2024.analysis.passes;
+
+import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
+import pt.up.fe.comp.jmm.ast.JmmNode;
+import pt.up.fe.comp.jmm.report.Report;
+import pt.up.fe.comp.jmm.report.Stage;
+import pt.up.fe.comp2024.analysis.AnalysisVisitor;
+import pt.up.fe.comp2024.ast.Kind;
+import pt.up.fe.comp2024.ast.NodeUtils;
+
+public class VarArgCheck extends AnalysisVisitor {
+    
+    @Override
+    public void buildVisitor() {
+        addVisit(Kind.METHOD_DECL, this::visitMethodDecl);
+    }
+    
+    private Void visitMethodDecl(JmmNode node, SymbolTable table) {
+        int lastIdx = table.getParameters(node.get("node")).size()-1;
+        for (int i = 0; i < table.getParameters(node.get("node")).size(); i++) {
+            if(node.get("isVarArg").equals("true") && (i == lastIdx)) {
+                return null;
+            }
+        }
+        var message = String.format("Variable '%s' does not exist.", node.get("id"));
+        addReport(Report.newError(
+                Stage.SEMANTIC,
+                NodeUtils.getLine(node),
+                NodeUtils.getColumn(node),
+                message,
+                null)
+        );
+        return null;
+    }
+}
