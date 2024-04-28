e81319164853b5a8841ae3b18cc160c38d8fbd23
Bartek Pacia <up202311689@fe.up.pt>
2024-04-28 20:54:53 +0100

    fix some more tests

--- src/main/pt/up/fe/comp2024/analysis/passes/VarArgCheck.java
+++ src/main/pt/up/fe/comp2024/analysis/passes/VarArgCheck.java
@@ -7,21 +7,21 @@ import pt.up.fe.comp.jmm.report.Stage;
 import pt.up.fe.comp2024.analysis.AnalysisVisitor;
 import pt.up.fe.comp2024.ast.Kind;
 import pt.up.fe.comp2024.ast.NodeUtils;
-import pt.up.fe.comp2024.ast.TypeUtils;
 
 public class VarArgCheck extends AnalysisVisitor {
-    
+
     @Override
     public void buildVisitor() {
         addVisit(Kind.METHOD_DECL, this::visitMethodDecl);
     }
-    
+
     private Void visitMethodDecl(JmmNode node, SymbolTable table) {
         final var params = table.getParameters(node.get("name"));
-        final var lastIdx = params.size()-1;
+        final var lastIdx = params.size() - 1;
         for (var param : params) {
             var paramType = param.getType().getName();
-            if((params.get(lastIdx) != param) && (paramType.equals("int..."))) {
+            System.out.println("EHHEHEEH paramType is: " + paramType);
+            if ((params.get(lastIdx) != param) && (paramType.equals("int..."))) {
                 var message = String.format("Variable '%s' does not exist - vararg_methodvisit", node);
                 addReport(Report.newError(
                         Stage.SEMANTIC,
