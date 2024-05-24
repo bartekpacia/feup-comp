932fa05c3ce856435ff6e2942481ca098c869f9f
Bartek Pacia <up202311689@fe.up.pt>
2024-05-24 20:40:52 +0100

    investigating how to fix OllirTest.importsComplex

--- src/main/pt/up/fe/comp2024/backend/JasminGenerator.java
+++ src/main/pt/up/fe/comp2024/backend/JasminGenerator.java
@@ -290,7 +290,12 @@ public class JasminGenerator {
             }
             case invokestatic -> {
                 // final String classname = ((ClassType) callInst.getCaller().getType()).getName();
-                final String classname = ((Operand) callInst.getCaller()).getName();
+                var classname = ((Operand) callInst.getCaller()).getName();
+                // If this classname is , put whole path
+                for (final var imp : ollirResult.getSymbolTable().getImports()) {
+                    System.out.println("DEBUG imp: " + imp);
+                }
+
                 final String methodname = ((LiteralElement) callInst.getMethodName()).getLiteral().replace("\"", "");
                 final String descriptor = "(" + JasminUtils.argumentsToDescriptor(callInst.getArguments()) + ")V";
 
